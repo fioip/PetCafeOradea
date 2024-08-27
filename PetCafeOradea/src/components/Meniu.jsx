b4e7f838592a7eb21c94a193cce7dce133ae364f
Iuhasz P. Fiona <fio.iuhasz@gmail.com>
2024-08-27 23:58:23 +0300

    navbar component & route to the pages

---
+++ PetCafeOradea/src/components/Meniu.jsx
@@ -0,0 +1,10 @@
+function Meniu() {
+  return (
+    <div className="text-2xl text-black h-full bg-cyan-900">
+      <div className="text-2xl text-black h-full bg-green-900">Meniu</div>
+      <div className="text-2xl text-black h-full bg-green-500">Meniu</div>
+    </div>
+  );
+}
+
+export default Meniu;
