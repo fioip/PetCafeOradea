aa36eaa6c39940b2686ddc8ecbbeb9a0d4873a7f
Iuhasz P. Fiona <fio.iuhasz@gmail.com>
2024-10-17 22:05:27 +0300

    update pagini

--- PetCafeOradea/src/components/Contact.jsx
+++ PetCafeOradea/src/components/Contact.jsx
@@ -9,119 +9,104 @@ import { FaTiktok } from "react-icons/fa";
 
 function Contact() {
   return (
-    <div className="md:ml-[15%] lg:ml-[10%] xl:ml-[10%] h-screen">
-      <div className="bg-[#FFF8EA]">
-        <Banner />
+    <div className="md:ml-[25%] lg:ml-[25%] xl:ml-[20%] h-full">
+      <div className="flex-1 flex flex-col items-center justify-start bg-[#FFF8EA]">
+        <div className="w-full">
+          {" "}
+          <Banner />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 justify-center items-center mt-16">
+            {/* Phone Section */}
+            <div className="flex flex-col justify-center items-center space-y-7">
+              <FaPhone className="text-[#633404] w-20 h-20" />
+              <p className="border-b-4 border-[#EF7F00] pb-4 md:text-3xl text-2xl font-medium text-[#633404]">
+                Telefon
+              </p>
+              <p className="font-semibold text-2xl md:text-2xl lg:text-2xl text-[#EF7F00]">
+                0712 123 123
+              </p>
+            </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center mt-16">
-          {/* Phone Section */}
-          <div className="flex flex-col justify-center items-center space-y-4">
-            <FaPhone className="text-[#633404] text-6xl" />
-            <p className="border-b-4 border-[#EF7F00] pb-4 md:text-3xl text-2xl font-medium text-[#633404]">
-              Telefon
-            </p>
-            <p className="font-semibold text-2xl md:text-3xl lg:text-2xl text-[#EF7F00]">
-              0712 123 123
-            </p>
-          </div>
+            {/* Location Section */}
+            <div className="flex flex-col justify-center items-center space-y-4">
+              <FaLocationDot className="text-[#633404] w-20 h-20" />
+              <p className="border-b-4 border-[#EF7F00] pb-4 md:text-3xl text-2xl font-medium text-[#633404]">
+                Locație
+              </p>
+              <p className="font-semibold text-2xl md:text-2xl lg:text-2xl text-[#EF7F00] flex flex-col text-center">
+                Piața 1 Decembrie
+                <span>Nr. 12, Oradea</span>
+              </p>
+            </div>
 
-          {/* Email Section */}
-          <div className="flex flex-col justify-center items-center space-y-4">
-            <IoMdMail className="text-[#633404] text-6xl" />
-            <p className="border-b-4 border-[#EF7F00] pb-4 md:text-3xl text-2xl font-medium text-[#633404]">
-              Mail
-            </p>
-            <p className="font-semibold text-2xl md:text-3xl lg:text-2xl text-[#EF7F00]">
-              pet.cafe@yahoo.com
-            </p>
-          </div>
+            {/* Email Section */}
+            <div className="flex flex-col justify-center items-center space-y-4">
+              <IoMdMail className="text-[#633404] w-20 h-20" />
+              <p className="border-b-4 border-[#EF7F00] pb-4 md:text-3xl text-2xl font-medium text-[#633404]">
+                Mail
+              </p>
+              <p className="font-semibold text-2xl md:text-2xl lg:text-2xl text-[#EF7F00]">
+                pet.cafe@yahoo.com
+              </p>
+            </div>
 
-          {/* Location Section */}
-          <div className="flex flex-col justify-center items-center space-y-4">
-            <FaLocationDot className="text-[#633404] text-6xl" />
-            <p className="border-b-4 border-[#EF7F00] pb-4 md:text-3xl text-2xl font-medium text-[#633404]">
-              Locație
-            </p>
-            <p className="font-semibold text-2xl md:text-3xl lg:text-2xl text-[#EF7F00] flex flex-col text-center">
-              Piața 1 Decembrie
-              <span>Nr. 12, Oradea</span>
-            </p>
-          </div>
+            {/* Instagram Section */}
+            <div className="flex flex-col justify-center items-center space-y-4">
+              <FaInstagram className="text-[#633404] text-6xl" />
+              <p className="border-b-4 border-[#EF7F00] pb-4 md:text-3xl text-2xl font-medium text-[#633404]">
+                Instagram
+              </p>
+              <p className="font-semibold text-2xl md:text-2xl lg:text-2xl text-[#EF7F00] text-center">
+                petcafeoradea
+              </p>
+            </div>
 
-          {/* Instagram Section */}
-          <div className="flex flex-col justify-center items-center space-y-4">
-            <FaInstagram className="text-[#633404] text-6xl" />
-            <p className="border-b-4 border-[#EF7F00] pb-4 md:text-3xl text-2xl font-medium text-[#633404]">
-              Instagram
-            </p>
-            <p className="font-semibold text-2xl md:text-3xl lg:text-2xl text-[#EF7F00] text-center">
-              petcafeoradea
-            </p>
-          </div>
+            {/* Facebook Section */}
+            <div className="flex flex-col justify-center items-center space-y-4">
+              <FaFacebook className="text-[#633404] text-6xl" />
+              <p className="border-b-4 border-[#EF7F00] pb-4 md:text-3xl text-2xl font-medium text-[#633404]">
+                Facebook
+              </p>
+              <p className="font-semibold text-2xl md:text-2xl lg:text-2xl text-[#EF7F00] text-center">
+                Pet Cafe Oradea
+              </p>
+            </div>
 
-          {/* Facebook Section */}
-          <div className="flex flex-col justify-center items-center space-y-4">
-            <FaFacebook className="text-[#633404] text-6xl" />
-            <p className="border-b-4 border-[#EF7F00] pb-4 md:text-3xl text-2xl font-medium text-[#633404]">
-              Facebook
-            </p>
-            <p className="font-semibold text-2xl md:text-3xl lg:text-2xl text-[#EF7F00] text-center">
-              Pet Cafe Oradea
-            </p>
+            {/* Tik Tok Section */}
+            <div className="flex flex-col justify-center items-center space-y-4">
+              <FaTiktok className="text-[#633404] text-6xl" />
+              <p className="border-b-4 border-[#EF7F00] pb-4 md:text-3xl text-2xl font-medium text-[#633404]">
+                Tik Tok
+              </p>
+              <p className="font-semibold text-2xl md:text-2xl lg:text-2xl text-[#EF7F00] text-center">
+                petcafeoradea
+              </p>
+            </div>
           </div>
-
-          {/* Tik Tok Section */}
-          <div className="flex flex-col justify-center items-center space-y-4">
-            <FaTiktok className="text-[#633404] text-6xl" />
-            <p className="border-b-4 border-[#EF7F00] pb-4 md:text-3xl text-2xl font-medium text-[#633404]">
-              Tik Tok
-            </p>
-            <p className="font-semibold text-2xl md:text-3xl lg:text-2xl text-[#EF7F00] text-center">
-              petcafeoradea
-            </p>
+          {/* Cafe description */}
+          <div className="mt-12">
+            <h2
+              className="text-justify leading-7 md:leading-relaxed lg:leading-9 sm:leading-6 text-lg 
+                md:text-3xl lg:text-xl md:mr-12 lg:mr-16 mr-10 ml-10 md:ml-12 lg:ml-16 text-[#633404] font-medium"
+            >
+              “Pet Cafe” este un concept nou în Oradea, inspirat din cultura
+              asiatică, unde animalele sunt considerate parte din familie. În
+              localul nostru puteți savura diferite băuturi pe bază de cafea dar
+              nu numai, alături bineînțeles de patrupezi foarte gingași și
+              prietenoși. Cafeneaua noastră are două zone predestinate
+              consumului, una dintre ele fiind “împânzită” de cățeluși simpatici
+              și energici, iar a doua zonă este “păzită” de feline blănoase și
+              jucăușe.
+            </h2>
           </div>
+          {/* Google Maps iframe */}
+          <div className="w-full mt-10 mb-10">
+            <iframe
+              className="w-full h-[500px]"
+              src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Pet%20Cafe,%20Oradea,%20%20Bihor+(Pet%20Cafe)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+            ></iframe>
+          </div>
+          <Footer />
         </div>
-
-        {/* Cafe description */}
-        <div className="mt-12">
-          <h2
-            className="text-justify leading-7 md:leading-relaxed lg:leading-9 sm:leading-6 text-lg 
-          md:text-3xl lg:text-xl md:mr-12 lg:mr-16 mr-10 ml-10 md:ml-12 lg:ml-16 text-[#633404] font-medium "
-          >
-            “Pet Cafe” este un concept nou în Oradea, inspirat din cultura
-            asiatică, unde animalele sunt considerate parte din familie. În
-            localul nostru puteți savura diferite băuturi pe bază de cafea dar
-            nu numai, alături bineînțeles de patrupezi foarte gingași și
-            prietenoși. Cafeneaua noastră are două zone predestinate consumului,
-            una dintre ele fiind “împânzită” de cățeluși simpatici și energici,
-            iar a doua zonă este “păzită” de feline blănoase și jucăușe. Orice
-            client e liber să aleagă în care zonă dorește să se așeze, urmând
-            să-i fie aduse la cunoștință regulile de ordine interioară legate de
-            animăluțe, aceste reguli aflându-se pe fiecare masă pentru ca orice
-            client să fie informat legat de acestea. Odată ce pășești în
-            interior, te întâmpină o atmosferă caldă și primitoare, unde decorul
-            cozy, cu mobilier confortabil și detalii prietenoase, te face să te
-            simți imediat ca acasă. Luminile moi și muzica de fundal adaugă o
-            notă de intimitate, invitându-te să te așezi și să te bucuri de
-            momentele petrecute aici. Personalul dedicat și prietenos este mereu
-            gata să te ajute să alegi cea mai bună cafea dintr-o selecție
-            variată de băuturi, de la espresso-uri intense la latte-uri
-            cremoase. Fiecare ceașcă de cafea este preparată cu grijă, astfel
-            încât fiecare înghițitură să fie o explozie de arome, perfectă
-            pentru a însoți momentele de joacă și interacțiune cu prietenii
-            blănoși ai cafenelei.
-          </h2>
-        </div>
-
-        {/* Google Maps iframe */}
-        <div className="w-full mt-10 mb-10">
-          <iframe
-            className="w-full h-[500px]"
-            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Pet%20Cafe,%20Oradea,%20%20Bihor+(Pet%20Cafe)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-          ></iframe>
-        </div>
-
-        <Footer />
       </div>
     </div>
   );
